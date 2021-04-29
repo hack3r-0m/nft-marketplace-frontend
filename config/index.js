c2e9566038f48550842846d0e73cf671f6566073
nitinmittal23 <nitinmittal44066@gmail.com>
2021-04-29 16:47:34 +0530

    fix: node env

--- config/index.js
+++ config/index.js
@@ -3,14 +3,14 @@ import Vue from "vue";
 const config = require("./default");
 // debugger;
 console.log("BUILD_ENV", process.env.BUILD_ENV)
-if (process.env.BUILD_ENV === "testnet") {
-    Object.assign(config, require("./testnet"))
+if (process.env.BUILD_ENV === "mainnet" || process.env.NODE_ENV === "production") {
+    Object.assign(config, require("./mainnet"))
 }
 else if (process.env.BUILD_ENV === "staging") {
     Object.assign(config, require("./staging"))
 } 
 else {
-    Object.assign(config, require("./mainnet"))
+    Object.assign(config, require("./testnet"))
 }
 Vue.appConfig = config;
 // export config
